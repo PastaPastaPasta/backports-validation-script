8ec5264fe670e35071e35d4813c7171bd37cd41f
pasta <pasta@dashboost.org>
2021-08-30 17:17:13 -0400

    enhancements: add auto downloading, search all sheets, fix some bugs, remove .csv files from repo

--- main.py
+++ main.py
@@ -2,8 +2,28 @@ import csv
 import os
 from dataclasses import dataclass
 from enum import Enum
+import requests
 
 import git
+
+documet_id = "1DnKxat0S0H62CJOzXpKGPXTa8hgoVOjGYZzoClmGSB8"
+
+def download_sheet_as_csv(files):
+    print("downloading necessary google sheet files")
+    for file, gid in files:
+        print("downloading", file)
+        file_link = f"https://docs.google.com/spreadsheets/d/{documet_id}/export?gid={gid}&format=csv"
+        response = requests.get(file_link)
+        assert response.status_code == 200, 'Wrong status code'
+
+        if os.path.exists(file):
+            os.remove(file)
+
+        with open(file, "wb") as csvfile:
+            csvfile.write(response.content)
+
+    return
+
 
 repo = git.Repo
 if not os.path.isdir("dashpaydash"):
@@ -19,15 +39,18 @@ assert str(repo.head.reference) == "develop"
 repo.remotes.origin.pull()
 
 
-files = {
-    '0.16.csv',
-    '0.17.csv',
-    '0.18.csv',
-    '0.19.csv',
-    '0.20.csv',
-    '0.21.csv',
-    '0.22.csv'
-}
+
+files = [
+    ('0.16.csv', "1860904166"),
+    ('0.17.csv', "119635402"),
+    ('0.18.csv', "988587662"),
+    ('0.19.csv', "259176943"),
+    ('0.20.csv', "1507625552"),
+    ('0.21.csv', "331846632"),
+    ('0.22.csv', "1796444839")
+]
+
+download_sheet_as_csv(files)
 
 class StatusDone(Enum):
     DONE = 1
@@ -65,33 +88,33 @@ class backport_object:
 
 backport_objects = []
 
-# for file in files:
-with open('0.18.csv') as csvfile:
-    spamreader = csv.reader(csvfile, delimiter=',')
-    line = 0
-    for row in spamreader:
-        line += 1
+for file, _ in files:
+    with open(file) as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=',')
+        line = 0
+        for row in spamreader:
+            line += 1
 
-        if row[0] == "Status":
-            continue
+            if row[0] == "Status":
+                continue
 
-        # Skip fully blank lines
-        if row[0] == "" and row[1] == "" and row[2] == "" and row[3] == "":
-            continue
+            # Skip fully blank lines
+            if row[0] == "" and row[1] == "" and row[2] == "" and row[3] == "":
+                continue
 
-        obj = backport_object(StatusDone.NONE, StatusStaged.NONE, "", "", "", False, csvfile.name)
-        if row[0] == "DNM (Did Not Merge)":
-            obj.status_done = StatusDone.DNM
-        elif row[0] == "Done (Merged to dashpay)":
-            obj.status_done = StatusDone.DONE
+            obj = backport_object(StatusDone.NONE, StatusStaged.NONE, "", "", "", False, csvfile.name)
+            if row[0] == "DNM (Did Not Merge)":
+                obj.status_done = StatusDone.DNM
+            elif row[0] == "Done (Merged to dashpay)":
+                obj.status_done = StatusDone.DONE
 
-        if "Staged" in row[1]:
-            obj.status_staged = StatusStaged.STAGED
+            if "Staged" in row[1]:
+                obj.status_staged = StatusStaged.STAGED
 
-        obj.commit_hash = row[2]
-        obj.message = row[3]
+            obj.commit_hash = row[2]
+            obj.message = row[3]
 
-        backport_objects.append(obj)
+            backport_objects.append(obj)
 
 commit = repo.head.reference.commit
 
@@ -103,7 +126,7 @@ log_temp = repo.git.log("--oneline").split("\n")
 
 # This will filter off the commit id, and everything after first semicolon
 for v in log_temp:
-    if not ("Merge" in v or "bitcoin" in v or "Backport" in v or "backport" in v): continue
+    # if not ("Merge" in v or "merge" in v or "bitcoin" in v or "Backport" in v or "backport" in v): continue
     log.append(v.split(" ", 1)[1])
 
 print(len(log))
@@ -111,8 +134,9 @@ print(len(log))
 
 def search_for_merge_number(log_i, number, ignore_partial=True):
     for item in log_i:
-        if ("bitcoin #" + number) in item or ("bitcoin#" + number) in item or ("Merge #" + number) in item or ("Backport " + number) in item or ("backport " + number) in item or ("bitcoin " + number) in item:
-            if ignore_partial and ("partial" in item or "Partial" in item):
+        item = item.lower()
+        if ("bitcoin #" + number) in item or ("bitcoin#" + number) in item or ("merge #" + number) in item or ("backport " + number) in item or ("backport " + number) in item or ("bitcoin " + number) in item:
+            if ignore_partial and "partial" in item:
                 continue
             return True
     return False
@@ -146,7 +170,7 @@ for obj in backport_objects:
 
     if obj.status_done is StatusDone.DONE:
         if not search_for_merge_number(log, number):
-            print("Stated done, but not found for", obj)
+            print("Stated done, not found for: ", obj)
     else:
         if search_for_merge_number(log, number):
-            print("Stated not done / unknown, but found for", obj)
+            print("Stated NOT done, found for:", obj)
